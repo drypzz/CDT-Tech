a4d31f62b92f98f6bb7c44d258a73c9b6a3de302
drypzz <fdfjonfmd@gmail.com>
2024-03-01 21:29:15 -0300

    First Commit

---
+++ src/app/components/Home.tsx
@@ -0,0 +1,9 @@
+export default function HomePage(){
+    return (
+        <div>
+            <h1>CDT Texh</h1>
+            <p>Capacitação Digital para a Terceira Idade</p>
+        </div>
+    )
+};
+
---
+++ src/app/layout.styles.ts
@@ -0,0 +1,8 @@
+"use client";
+
+import styled from 'styled-components';
+
+export const MainContainer = styled.main`
+    width: 100%;
+    height: 100%;
+`;
